39f1006f8a87fe2bf5096d5133e6443deec9bbb3
S19S93 <S19S93@yandex.ru>
2021-12-08 17:04:09 +0200

    Master

---
+++ src/blocks/modules/header/header.js
@@ -0,0 +1,15 @@
+import Mmenu from 'mmenu-js';
+
+(function() {
+	new Mmenu( "#menu", {
+		"extensions": [
+		"pagedim-black",
+		"position-front"
+		],
+		"navbar": {
+			"title": "Logoipsum",
+			"sticky": false
+		},
+	});
+})();
+
