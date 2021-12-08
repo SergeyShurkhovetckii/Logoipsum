3de7ce351cffd43d4818a7e7db9744b780e6b45c
S19S93 <S19S93@yandex.ru>
2021-12-08 17:47:14 +0200

    Master

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -13,3 +13,19 @@ import Mmenu from 'mmenu-js';
 	});
 })();
 
+
+(function fixedMenuScroll() {
+    const header = document.querySelector('.header-scroll');
+
+    if (header) {
+        window.addEventListener('scroll', function () {
+            if (window.scrollY > 0) {
+                header.classList.add('header--fixed');
+
+            } else {
+                header.classList.remove('header--fixed');
+            }
+        })
+    }
+})();
+
