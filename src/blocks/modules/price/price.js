3e68a30f8894c832c46fc9e870e5bd5f738c9358
S19S93 <S19S93@yandex.ru>
2021-12-09 17:34:54 +0200

    Master

---
+++ src/blocks/modules/price/price.js
@@ -0,0 +1,19 @@
+(function radioButton() {
+    // Константы
+    const btn = document.querySelectorAll('price-top-btn');
+    let active = false;
+    // Ослеживаем событие открытия
+    if (btn) {
+        btn.addEventListener('click', toggle);
+        // Сам функция открытия 
+        function toggle() {
+            if (active == true){
+                btn.classList.remove('price--active');
+                active =false;
+            } else {
+                btn.classList.add('price--active');
+                active =false;
+            }
+        }
+    }
+})();
