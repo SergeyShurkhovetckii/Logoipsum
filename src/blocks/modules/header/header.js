d837ee669e27740030e2d8f9f2e2d28d488092be
S19S93 <S19S93@yandex.ru>
2021-12-14 17:55:01 +0200

    Master

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -16,14 +16,17 @@ import Mmenu from 'mmenu-js';
 
 (function fixedMenuScroll() {
     const header = document.querySelector('.header-scroll');
+    const heder_scroll = document.querySelector('.header');
 
-    if (header) {
+    if (header && heder_scroll ) {
         window.addEventListener('scroll', function () {
             if (window.scrollY > 0) {
                 header.classList.add('header--fixed');
+                heder_scroll.classList.add('header-sc');
 
             } else {
                 header.classList.remove('header--fixed');
+                heder_scroll.classList.remove('header-sc');
             }
         })
     }
--- src/blocks/modules/price/price.js
+++ src/blocks/modules/price/price.js
@@ -1,3 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let content = document.querySelectorAll('[data-toggle]');
+    let open_button = true;
+
+    for (let i = 0; i < content.length; i++) {
+        if (open_button == true) {
+            content[i].addEventListener('click', () => {
+                content[i].classList.toggle('price--active');
+                open_button == false;
+            })
+        }
+
+
+    }
+});
 
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -8,24 +23,25 @@ document.addEventListener('DOMContentLoaded', () => {
     let white = document.querySelectorAll('.price-card__info');
     let btn = document.querySelectorAll('.card-info__btn');
 
-    for (let i = 0; i < button.length && i < font.length && i <white.length && i < price.length
-         && i < btn.length && i <priceSpan.length; i++) {
-                button[i].addEventListener('mouseover', () => {
-                    button[i].classList.add('change');
-                    price[i].classList.add('white');
-                    priceSpan[i].classList.add('white');
-                    font[i].classList.add('white');
-                    white[i].classList.add('white');
-                    btn[i].classList.add('card-btn--hover');
-                    console.log(font[i]);
-                });
-            button[i].addEventListener('mouseout', () => {
-                button[i].classList.remove('change');
-                price[i].classList.remove('white');
-                priceSpan[i].classList.remove('white');
-                font[i].classList.remove('white');
-                white[i].classList.remove('white');
-                btn[i].classList.remove('card-btn--hover');
+    for (let i = 0; i < button.length && i < font.length && i < white.length && i < price.length &&
+        i < btn.length && i < priceSpan.length; i++) {
+        button[i].addEventListener('mouseover', () => {
+            button[i].classList.add('change');
+            price[i].classList.add('white');
+            priceSpan[i].classList.add('white');
+            font[i].classList.add('white');
+            white[i].classList.add('white');
+            btn[i].classList.add('card-btn--hover');
+            // console.log(font[i]);
+        });
+        button[i].addEventListener('mouseout', () => {
+            button[i].classList.remove('change');
+            price[i].classList.remove('white');
+            priceSpan[i].classList.remove('white');
+            font[i].classList.remove('white');
+            white[i].classList.remove('white');
+            btn[i].classList.remove('card-btn--hover');
 
-            })};      
+        })
+    };
 });
